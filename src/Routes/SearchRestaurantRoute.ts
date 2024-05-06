5ae5f000fe2c561a7ea7ab5ae173a2dce7106ca9
01fe22bca119 <dileepupare8@gmail.com>
2024-05-07 04:30:29 +0530

    backend changes

--- src/Routes/SearchRestaurantRoute.ts
+++ src/Routes/SearchRestaurantRoute.ts
@@ -4,13 +4,22 @@ import SearchRestaurantController from "../Controllers/SearchRestaurantControlle
 
 const router = express.Router();
 
-router.get("/search/:city", 
-       param("city")
-       .isString()
-       .trim()
-       .notEmpty()
-       .withMessage("city parameter must be a valid string"),
-        SearchRestaurantController .searchRestaurant
+router.get("/:restaurantId",
+        param("restaurantId")
+                .isString()
+                .trim()
+                .notEmpty()
+                .withMessage("RestaurantId parameter must be a valid string"),
+                SearchRestaurantController.getRestaurant
+        )
+
+router.get("/search/:city",
+        param("city")
+                .isString()
+                .trim()
+                .notEmpty()
+                .withMessage("city parameter must be a valid string"),
+        SearchRestaurantController.searchRestaurant
 );
 
 export default router;
